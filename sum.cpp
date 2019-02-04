ee77c1ef863337c966d10cad7ceb272dd2c08a23
Eric Dittus <ericdittus@Erics-MacBook-Air.local>
2019-02-04 11:53:19 -0500

    Project1A

--- sum.cpp
+++ sum.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int sum;
+    int sum = 0;
     int s;
-    int new_num;
-    cout << "Please input a series of numbers you want to sum: " << endl;
     while(cin >> s) {
       // While the reading operation is a success
-          new_num = s;
-          sum = sum + new_num;
+
+          sum = sum + s;
           // add following value to cummulative sum
     }
     cout << "Your sum is " << sum << endl;
