766776dd97da19ccca98d329ad22f476ad18cce6
Eric Dittus <ericdittus@Erics-MacBook-Air.local>
2019-02-04 09:43:53 -0500

    new

--- sum.cpp
+++ sum.cpp
@@ -3,10 +3,12 @@ using namespace std;
 int main() {
     int sum;
     int s;
+    int new_num;
     cout << "Please input a series of numbers you want to sum: " << endl;
     while(cin >> s) {
       // While the reading operation is a success
-          sum = sum + s;
+          new_num = s;
+          sum = sum + new_num;
           // add following value to cummulative sum
     }
     cout << "Your sum is " << sum << endl;
