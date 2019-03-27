557b32b332aee8a65b42ecf87e6a48e249acc2b9
Eric Dittus <ericdittus@Erics-MacBook-Air.local>
2019-03-27 19:31:12 -0400

    yup

--- hamming.cpp
+++ hamming.cpp
@@ -2,31 +2,32 @@
 #include <fstream>
 #include <cstdlib>
 #include <iostream>
-
+//putting in all includes
 using namespace std;
 
-int main(){
+int main(){ // main function
   ifstream fin ("mutations.txt");
-  if(fin.fail()){
+  if(fin.fail()){ //fin into file
     cerr <<"File cannot be read, opened, or does not exist.\n";
     exit(1);
   }
   string mut, orig;
   while(getline (fin, mut), getline (fin, orig)){
-    int count = 0;
-    for(int i = 0;i < 45; i++){
+    int count = 0; // while loop stuff
+    for(int i = 0;i < 45; i++){ //coolio
       if (mut[i] != orig[i]){
-        count += 1;
+        count += 1; //count + 1
       }
     }
     if (count == 17) {
-      cout << count << " no" << endl;
-      count = 0;
+      cout << count << " no" << endl; //couts no
+      count = 0; //must reset
+                //for next while
     } else if (count > 0){
-      cout << count << " yes" << endl;
-      count = 0;
+      cout << count << " yes" << endl; //couts yes
+      count = 0;//count is zero again
     } else{
-      cout << 0 << " no" << endl;
+      cout << 0 << " no" << endl; //couts no
       count = 0;
     }
   }
